890851b54c9b09ae9af867f4e80b242c43b61203
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-10-27 23:41:40 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  list.append(listItem);
+}
+
+
+
+
