fa9ea1e254185f5b9bf5cca6195fbd5eee9cde6f
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-10-30 13:46:46 +0100

    task-05

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+let input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", (event) => {
+  if (input.value === "") {
+    output.textContent = "Anonymous";
+  } else {
+    output.textContent = input.value;
+  }
+});
