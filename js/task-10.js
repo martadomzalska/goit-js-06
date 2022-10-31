e011d59d8d81ded44bb4c8ee19cce50f4c0a5efb
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-10-31 18:06:32 +0100

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes() {
+  let amount = input.value;
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = 30 + 10 * i + "px";
+    div.style.height = 30 + 10 * i + "px";
+    div.style.backgroundColor = getRandomHexColor();
+    boxes.append(div);
+  }
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
