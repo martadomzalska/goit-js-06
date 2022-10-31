b804752280babda23a2a9065093f7a4b9bb573ac
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-10-31 13:32:21 +0100

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const text = document.querySelector(".color");
+
+const changeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  text.textContent = getRandomHexColor();
+};
+
+btn.addEventListener("click", changeColor);
