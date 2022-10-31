183b4e2b74b4ec033c9c533f1bf630ea4783874b
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-10-31 13:13:06 +0100

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Proszę uzupełnić wszystkie pola!")
+    }
+    
+   console.log(`elements = { email:"${email.value}", password: "${password.value}",};`);
+    event.currentTarget.reset();
+}
