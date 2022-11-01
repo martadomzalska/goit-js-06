1de78ba73d7cc851d5c0d5da8ff237d3aa60bcf6
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-11-01 13:12:49 +0100

    task-07-update

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 let input = document.querySelector("#font-size-control");
 let text = document.querySelector("#text");
+text.style.fontSize = input.value + "px";
 
 input.addEventListener("input", () => {
     text.style.fontSize = input.value + "px";
