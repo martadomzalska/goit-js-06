cbb51cc637f1b5c60b134c5c08a682f2de568fc0
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-11-01 14:27:12 +0100

    update-task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,14 +1,22 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const input = document.querySelector("input");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxes = document.querySelector("#boxes");
+const controls = document.querySelector("#controls");
 
 function createBoxes() {
   let amount = input.value;
+  if (input.value > 50) {
+    alert("Proszę podać liczbę nie większą niż 50");
+    input.value = "";
+    return
+  }
   for (let i = 0; i < amount; i++) {
     const div = document.createElement("div");
     div.style.width = 30 + 10 * i + "px";
